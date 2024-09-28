5989544cce82ac608076edee938f5a69e0dba34a
okandrms <okanndrms@gmail.com>
2024-09-28 15:28:47 +0200

    login

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -27,3 +27,11 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Frontend');
   });
 });
+
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
+function expect(app: AppComponent) {
+  throw new Error('Function not implemented.');
+}
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,51 +1,39 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { WordFormComponent } from './wordform/wordform.component';
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule, // Add CommonModule here
-    RouterOutlet,
-    NavbarComponent,
-    LoginComponent,
-    WordFormComponent
-  ],
-  template: `
-    <ng-template *ngIf="isLoggedIn">
-      <app-navbar></app-navbar>
-    </ng-template>
-
-    <ng-template *ngIf="!isLoggedIn">
-      <app-login></app-login>
-    </ng-template>
-
-    <router-outlet></router-outlet>
-
-    <h1>{{ title }}</h1>
-
-    <app-wordform></app-wordform>
-  `,
+  imports: [ CommonModule, RouterOutlet,LoginComponent, WordFormComponent, RegisterComponent],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'LanguagePractice';
   isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
+    // Check user login status from AuthService
     this.isLoggedIn = this.authService.isLoggedIn();
+
+    // If user is not logged in, redirect to login page
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/login']);  // Redirect to login page if not logged in
+    }
   }
 
   logout() {
     this.authService.logout().subscribe(() => {
       this.isLoggedIn = false;
+      this.router.navigate(['/login']);  // Redirect to login page upon logout
     });
   }
 }
